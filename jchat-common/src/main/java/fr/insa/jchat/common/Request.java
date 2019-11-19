2ed6a83c73178aac40fcf9adf7487972a99a54f0
Pacôme <pacome.lehoux@laposte.net>
2019-11-19 10:05:21 +0100

    Moved Request helper methods to Request class

--- jchat-common/src/main/java/fr/insa/jchat/common/Request.java
+++ jchat-common/src/main/java/fr/insa/jchat/common/Request.java
@@ -1,17 +1,109 @@
 package fr.insa.jchat.common;
 
+import fr.insa.jchat.common.exception.InvalidBodySizeException;
+import fr.insa.jchat.common.exception.InvalidMethodException;
+import fr.insa.jchat.common.exception.InvalidParamValue;
+import fr.insa.jchat.common.exception.InvalidRequestException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Request {
+    private static final Logger LOGGER = LogManager.getLogger(Request.class);
+
     private Method method;
 
     private Map<String, String> params;
 
     private String body;
+
+    public static Request createRequestFromReader(BufferedReader in) throws IOException, InvalidParamValue, InvalidBodySizeException, InvalidMethodException {
+        Request request = new Request();
+
+        // read request method
+        String line = in.readLine();
+        LOGGER.debug("Method : {}", line);
+        try {
+            request.setMethod(Request.Method.valueOf(line));
+        }
+        catch(IllegalArgumentException e) {
+            throw new InvalidMethodException(line);
+        }
+
+        // read request params
+        line = in.readLine();
+        while(line.length() > 0) {
+            String[] param = line.split(":", 2);
+            LOGGER.debug("Param : {}", (Object)param);
+            request.setParams(param[0], param[1]);
+            line = in.readLine();
+        }
+
+        // handle body if length param is present
+        try {
+            if(request.hasParam("length")) {
+                // parse length value
+                int length = Integer.parseInt(request.getParam("length"));
+                if(length < 0)
+                    throw new NumberFormatException("For input string : " + length);
+
+                // read that many characters from input
+                char[] body = new char[length];
+                int nbRead = in.read(body, 0, length);
+
+                // check that enough characters were read
+                if(nbRead != length)
+                    throw new InvalidBodySizeException(length, nbRead);
+
+                LOGGER.debug("Body : {}", (Object)body);
+                request.setBody(new String(body));
+            }
+        }
+        catch(NumberFormatException e) {
+            throw new InvalidParamValue("length param must be strictly greater than 0", e, "length");
+        }
+
+        return request;
+    }
+
+    public static Request createErrorResponse(InvalidRequestException e) {
+        Request request = new Request();
+        request.setMethod(Request.Method.ERROR);
+        request.setParams("errorName", e.getErrorName());
+
+        try {
+            Field[] fields = e.getClass().getDeclaredFields();
+            for(Field field : fields) {
+                field.setAccessible(true);
+                request.setParams(field.getName(), field.get(e).toString());
+            }
+        }
+        catch(IllegalAccessException ex) {
+            LOGGER.error("An error occured while creating error response", ex);
+        }
+
+        return request;
+    }
+
+    public static void sendResponse(Request response, PrintStream out) {
+        out.println(response.getMethod());
+
+        for(String param : response.getParamNames())
+            out.println(param + ":" + response.getParam(param));
+
+        out.println();
+
+        if(response.getBody() != null && response.getBody().length() > 0)
+            out.println(response.getBody());
+    }
 
     public Request() {
         this.params = new HashMap<>();
--- jchat-server/src/main/java/fr/insa/jchat/server/ClientThread.java
+++ jchat-server/src/main/java/fr/insa/jchat/server/ClientThread.java
@@ -4,7 +4,6 @@ import fr.insa.jchat.common.Request;
 import fr.insa.jchat.common.exception.InvalidBodySizeException;
 import fr.insa.jchat.common.exception.InvalidMethodException;
 import fr.insa.jchat.common.exception.InvalidParamValue;
-import fr.insa.jchat.common.exception.InvalidRequestException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.lang.reflect.Field;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
@@ -35,89 +33,21 @@ public class ClientThread extends Thread {
 
             // try catch to handle request errors that uses the I/O streams
             try {
-                Request request = this.createRequest(socketIn);
+                Request request = Request.createRequestFromReader(socketIn);
                 LOGGER.debug("Got request : {}", request);
                 response = this.handleRequest(request);
             }
             catch(InvalidBodySizeException | InvalidParamValue | InvalidMethodException e) {
-                response = this.createErrorResponse(e);
+                response = Request.createErrorResponse(e);
             }
 
-            this.sendResponse(response, socketOut);
+            Request.sendResponse(response, socketOut);
         }
         catch(IOException e) {
             LOGGER.error("An error occurred in client thread {}", this.getName(), e);
         }
 
         this.close();
-    }
-
-    private Request createRequest(BufferedReader in) throws IOException, InvalidParamValue, InvalidBodySizeException, InvalidMethodException {
-        Request request = new Request();
-
-        // read request method
-        String line = in.readLine();
-        LOGGER.debug("Method : {}", line);
-        try {
-            request.setMethod(Request.Method.valueOf(line));
-        }
-        catch(IllegalArgumentException e) {
-            throw new InvalidMethodException(line);
-        }
-
-        // read request params
-        line = in.readLine();
-        while(line.length() > 0) {
-            String[] param = line.split(":", 2);
-            LOGGER.debug("Param : {}", (Object)param);
-            request.setParams(param[0], param[1]);
-            line = in.readLine();
-        }
-
-        // handle body if length param is present
-        try {
-            if(request.hasParam("length")) {
-                // parse length value
-                int length = Integer.parseInt(request.getParam("length"));
-                if(length < 0)
-                    throw new NumberFormatException("For input string : " + length);
-
-                // read that many characters from input
-                char[] body = new char[length];
-                int nbRead = in.read(body, 0, length);
-
-                // check that enough characters were read
-                if(nbRead != length)
-                    throw new InvalidBodySizeException(length, nbRead);
-
-                LOGGER.debug("Body : {}", (Object)body);
-                request.setBody(new String(body));
-            }
-        }
-        catch(NumberFormatException e) {
-            throw new InvalidParamValue("length param must be strictly greater than 0", e, "length");
-        }
-
-        return request;
-    }
-
-    private Request createErrorResponse(InvalidRequestException e) {
-        Request request = new Request();
-        request.setMethod(Request.Method.ERROR);
-        request.setParams("errorName", e.getErrorName());
-
-        try {
-            Field[] fields = e.getClass().getDeclaredFields();
-            for(Field field : fields) {
-                field.setAccessible(true);
-                request.setParams(field.getName(), field.get(e).toString());
-            }
-        }
-        catch(IllegalAccessException ex) {
-            LOGGER.error("An error occured while creating error response", ex);
-        }
-
-        return request;
     }
 
     private Request handleRequest(Request request) {
@@ -132,17 +62,6 @@ public class ClientThread extends Thread {
         request1.setParams("foo", "bar");
         request1.setBody("lorem ipsum dolor sit amet");
         return request1;
-    }
-
-    private void sendResponse(Request response, PrintStream out) {
-        out.println(response.getMethod());
-
-        for(String param : response.getParamNames())
-            out.println(param + ":" + response.getParam(param));
-
-        out.println();
-
-        out.println(response.getBody());
     }
 
     private void close() {
