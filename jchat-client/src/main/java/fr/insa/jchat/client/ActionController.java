d08819d50a73353cb15605fec849f10bee76d492
Pacôme <pacome.lehoux@laposte.net>
2019-11-20 16:52:03 +0100

    Implemented login and modified register to automatically login on success

--- jchat-client/src/main/java/fr/insa/jchat/client/ActionController.java
+++ jchat-client/src/main/java/fr/insa/jchat/client/ActionController.java
@@ -12,11 +12,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.UUID;
 
 public class ActionController {
     private static final Logger LOGGER = LogManager.getLogger(ActionController.class);
 
     private static ConnectPane connectPane;
+
+    private static String ip;
+
+    private static int port;
+
+    private static UUID session;
 
     public static void setConnectPane(ConnectPane connectPane) {
         ActionController.connectPane = connectPane;
@@ -25,18 +32,18 @@ public class ActionController {
     public static void handleAction(String action) {
         switch(action) {
             case "register":
-                handleRegister();
+                doRegister();
                 break;
             case "login":
-                handleLogin();
+                doLogin();
                 break;
         }
     }
 
-    public static void handleRegister() {
+    public static void doRegister() {
         LOGGER.info("Register");
-        String ip = connectPane.getValue("ip");
-        int port = Integer.parseInt(connectPane.getValue("port"));
+        ip = connectPane.getValue("ip");
+        port = Integer.parseInt(connectPane.getValue("port"));
 
         try(
             Socket socket = new Socket(ip, port);
@@ -52,13 +59,48 @@ public class ActionController {
 
             Request response = Request.createRequestFromReader(socketIn);
             LOGGER.info("{}", response);
+
+            if(response.getMethod() == Request.Method.OK) {
+                connectPane.displayMessage("Successfully registered");
+                doLogin();
+            }
+            else if(response.getMethod() == Request.Method.ERROR)
+                connectPane.displayMessage(response.getParam("errorName"), true);
         }
         catch(IOException | InvalidBodySizeException | InvalidMethodException | InvalidParamValue e) {
             LOGGER.error(e.getMessage(), e);
         }
     }
 
-    public static void handleLogin() {
+    public static void doLogin() {
         LOGGER.info("Login");
+        ip = connectPane.getValue("ip");
+        port = Integer.parseInt(connectPane.getValue("port"));
+
+        try(
+            Socket socket = new Socket(ip, port);
+            PrintStream socketOut = new PrintStream(socket.getOutputStream());
+            BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            Request request = new Request();
+            request.setMethod(Request.Method.LOGIN);
+            request.setParam("username", connectPane.getValue("username"));
+            request.setParam("password", connectPane.getValue("password"));
+
+            Request.sendRequest(request, socketOut);
+
+            Request response = Request.createRequestFromReader(socketIn);
+            LOGGER.info("{}", response);
+
+            if(response.getMethod() == Request.Method.OK) {
+                session = UUID.fromString(response.getParam("session"));
+                connectPane.displayMessage("Successfully logged in");
+            }
+            else if(response.getMethod() == Request.Method.ERROR)
+                connectPane.displayMessage(response.getParam("errorName"), true);
+        }
+        catch(IOException | IllegalArgumentException | InvalidBodySizeException | InvalidMethodException | InvalidParamValue e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
