acc1173a34ec4dbe18f9eafbd9582365b3234af4
Pacôme <pacome.lehoux@laposte.net>
2019-11-21 15:19:50 +0100

    Wrapped UserListWidget content in a ScrollPane

--- jchat-client/src/main/java/fr/insa/jchat/client/widgets/UserListWidget.java
+++ jchat-client/src/main/java/fr/insa/jchat/client/widgets/UserListWidget.java
@@ -2,16 +2,25 @@ package fr.insa.jchat.client.widgets;
 
 import fr.insa.jchat.common.User;
 import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
 
-public class UserListWidget extends VBox {
+public class UserListWidget extends TitledPane {
     private List<User> users;
 
     private Insets margin;
 
     public UserListWidget(List<User> users) {
+        super();
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        VBox root = new VBox();
+
         this.users = users;
         this.margin = new Insets(0, 5, 5, 5);
 
@@ -19,8 +28,13 @@ public class UserListWidget extends VBox {
             .stream()
             .map(UserWidget::new)
             .forEach(userWidget -> {
-                this.getChildren().add(userWidget);
-                setMargin(userWidget, this.margin);
+                root.getChildren().add(userWidget);
+                VBox.setMargin(userWidget, margin);
             });
+
+        scrollPane.setContent(root);
+        this.setContent(scrollPane);
+        this.setText("Users");
+        this.setCollapsible(false);
     }
 }
