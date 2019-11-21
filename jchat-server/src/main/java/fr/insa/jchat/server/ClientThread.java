e29ce0fe9f8420f24cb59dfceaf33841a474e76d
Felix <felix.castillon@free.fr>
2019-11-21 20:28:39 +0100

    VIM MARCHE HALLELUJA

--- jchat-server/src/main/java/fr/insa/jchat/server/ClientThread.java
+++ jchat-server/src/main/java/fr/insa/jchat/server/ClientThread.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
-
+ 
 public class ClientThread extends Thread {
     private static final Logger LOGGER = LogManager.getLogger(ClientThread.class);
 
