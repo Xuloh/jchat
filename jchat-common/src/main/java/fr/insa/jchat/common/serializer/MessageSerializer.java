f523af9d88a85e693facdf3013f50a4632772326
Pacôme <pacome.lehoux@laposte.net>
2019-11-22 08:48:17 +0100

    Modified MessageSerializer to handle null values

--- jchat-common/src/main/java/fr/insa/jchat/common/serializer/MessageSerializer.java
+++ jchat-common/src/main/java/fr/insa/jchat/common/serializer/MessageSerializer.java
@@ -2,6 +2,7 @@ package fr.insa.jchat.common.serializer;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
@@ -9,13 +10,22 @@ import com.google.gson.JsonSerializer;
 import fr.insa.jchat.common.Message;
 
 import java.lang.reflect.Type;
+import java.util.Optional;
+import java.util.UUID;
 
 public class MessageSerializer implements JsonSerializer<Message> {
 
     @Override
     public JsonElement serialize(Message message, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject result = new JsonObject();
-        result.add("uuid", new JsonPrimitive(message.getUuid().toString()));
+
+        Optional
+            .ofNullable(message.getUuid())
+            .ifPresentOrElse(
+                uuid -> result.add("uuid", new JsonPrimitive(uuid.toString())),
+                () -> result.add("uuid", JsonNull.INSTANCE)
+            );
+
         result.add("text", new JsonPrimitive(message.getText()));
 
         JsonArray userTags = new JsonArray();
@@ -32,7 +42,12 @@ public class MessageSerializer implements JsonSerializer<Message> {
         JsonPrimitive recipient = message.getRecipient() == null ? null : new JsonPrimitive(message.getRecipient().getUsername());
         result.add("recipient", recipient);
 
-        result.add("date", new JsonPrimitive(message.getDate().getTimeInMillis()));
+        Optional
+            .ofNullable(message.getDate())
+            .ifPresentOrElse(
+                date -> result.add("date", new JsonPrimitive(date.getTimeInMillis())),
+                () -> result.add("date", JsonNull.INSTANCE)
+            );
 
         return result;
     }
