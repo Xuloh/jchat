5d6f432a0a8c6f19c782ab031d43a9ee7bf023e5
Pacôme <pacome.lehoux@laposte.net>
2019-11-23 19:26:56 +0100

    Fixed message text field not taking all the space it could

--- jchat-client/src/main/java/fr/insa/jchat/client/widgets/InputMessageWidget.java
+++ jchat-client/src/main/java/fr/insa/jchat/client/widgets/InputMessageWidget.java
@@ -2,28 +2,23 @@ package fr.insa.jchat.client.widgets;
 
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class InputMessageWidget extends HBox {
-    private Insets margin;
-
     private InputTextField textField;
 
     private ActionButton sendButton;
 
     public InputMessageWidget() {
-        this.margin = new Insets(5, 5, 5, 0);
         this.textField = new InputTextField("Say something :D");
+        this.textField.setPromptText("Send a message ...");
         this.sendButton = new ActionButton("Send", "Send your message", "send-message");
-        this.textField.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
-            if(event.getCode() == KeyCode.ENTER)
-                this.sendButton.fire();
-        });
+        this.textField.addEventHandler(ActionEvent.ACTION, event -> this.sendButton.fire());
         this.getChildren().addAll(this.textField, this.sendButton);
-        setMargin(this.textField, this.margin);
-        setMargin(this.sendButton, this.margin);
+        setHgrow(this.textField, Priority.ALWAYS);
+        setMargin(this.textField, new Insets(5, 0, 5, 5));
+        setMargin(this.sendButton, new Insets(5, 5, 5, 5));
     }
 
     public String getText() {
